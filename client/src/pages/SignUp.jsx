cc027eb02285a23d296d6e683bb9349e62bf9dad
DeepBhatt <deepbhatt88666@gmail.com>
2025-03-01 14:41:23 +0530

    Integrate sign-up functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,19 +1,83 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { set } from "mongoose";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      set
+      const response = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {        
+        const data = await response.json();
+        console.log(data);
+        setLoading(false);
+        setError(false);
+      } else {
+        setLoading(false);
+        setError(true);
+      }
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-        <form className='flex flex-col gap-4'>
-          <input type='text' placeholder='username' id='username' className='bg-slate-100 p-3 rounded-lg'/>
-          <input type='email' placeholder='email' id='email' className='bg-slate-100 p-3 rounded-lg'/>
-          <input type='password' placeholder='password' id='password' className='bg-slate-100 p-3 rounded-lg'/>
-          <button className='bg-gray-600 text-white p-3 rounded-lg uppercase hover:opacity-80 disabled:opacity-20'>Sign Up</button>
-        </form>
-        <div className='mt-3'>
-          <p>Already have an account? <Link to="/sign-in" className='text-blue-600'>Sign In</Link></p>
-        </div>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button disabled={loading} className="bg-gray-600 text-white p-3 rounded-lg uppercase hover:opacity-80 disabled:opacity-20">
+          {loading ? "Loading..." : "Sign Up"}
+        </button>
+      </form>
+      <div className="mt-3">
+        <p>
+          Already have an account?{" "}
+          <Link to="/sign-in" className="text-blue-600">
+            Sign In
+          </Link>
+        </p>
+      </div>
+      <p className="text-red-600">{error && "Something went wrong"}</p>
     </div>
-  )
+  );
 }
